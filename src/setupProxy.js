0f774a269fd4b0737061f6e5caa83c286a216188
Austin Johnson <austintjohnson92@gmail.com>
2022-10-07 15:46:23 -0700

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'portfolio-2xbusbofx-austin-t-johnson.vercel.app',
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'portfolio-2xbusbofx-austin-t-johnson.vercel.app',
+//       changeOrigin: true,
+//     })
+//   );
+// };
